8ea502637f01e29061e2804444f2dbef63a2b003
Ben Standefer <benstandefer@gmail.com>
2019-03-01 17:31:30 -0800

    Cleanup comment

--- lib/omniauth/strategies/atlassian_oauth2.rb
+++ lib/omniauth/strategies/atlassian_oauth2.rb
@@ -3,23 +3,9 @@
 require 'omniauth/strategies/oauth2'
 require 'uri'
 
-# NOTE: %3A in scope is a colon
+# Potential scopes: https://developer.atlassian.com/cloud/jira/platform/scopes/
+# offline_access read:jira-user read:jira-work write:jira-work manage:jira-project manage:jira-configuration
 #
-# https://auth.atlassian.com/authorize?
-# audience=api.atlassian.com
-# client_id=7v6ghRsOM5BIHj4jmCxEmxyL743hwTEq
-# scope=read%3Ajira-user
-# redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fatlassian%2Fcallback
-# state=${YOUR_USER_BOUND_VALUE}
-# response_type=code
-# prompt=consent
-
-# Scopes: https://developer.atlassian.com/cloud/jira/platform/scopes/
-# read:jira-user
-# read:jira-work
-# write:jira-work
-# manage:jira-project
-# manage:jira-configuration
 # Separate scopes with a space (%20)
 # https://developer.atlassian.com/cloud/jira/platform/oauth-2-authorization-code-grants-3lo-for-apps/
 
